4d310acc27ea0d75895592d68ab65e2fcbb566ad
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-03 13:14:22 -0400

    Update ClubMembers.java

--- ClubMembers.java
+++ ClubMembers.java
@@ -7,21 +7,14 @@ public class ClubMembers {
         memberList = new ArrayList<>();
     }
 
-    /**
-     * Adds new club members to memberList, as described in part (a).
-     * Precondition: names is a non-empty array.
-     */
+
     public void addMembers(String[] names, int gradYear) {
         for (String name : names) {
-            memberList.add(new MemberInfo(name, gradYear, true)); // Assuming new members start in good standing
+            memberList.add(new MemberInfo(name, gradYear, true)); 
         }
     }
 
-    /**
-     * Removes members who have graduated and returns a list of members who have
-     * graduated
-     * and are in good standing, as described in part (b).
-     */
+
     public ArrayList<MemberInfo> removeMembers(int year) {
         ArrayList<MemberInfo> graduatedGoodStanding = new ArrayList<>();
 
@@ -32,22 +25,18 @@ public class ClubMembers {
                     graduatedGoodStanding.add(member);
                 }
                 memberList.remove(i);
-                i--; // Decrement the index after removing an element
+                i--; 
             }
         }
         return graduatedGoodStanding;
     }
 
-    /**
-     * Returns the list of all club members.
-     */
+
     public ArrayList<MemberInfo> getMemberList() {
         return memberList;
     }
 
-    /**
-     * Sets the list of all club members.
-     */
+   
     public void setMemberList(ArrayList<MemberInfo> members) {
         memberList = members;
     }
