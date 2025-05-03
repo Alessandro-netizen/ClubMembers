ccda5e8fa4d76b4707ae8c238f51829e75fbf1a0
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-03 13:15:21 -0400

    Update MemberInfo.java

--- MemberInfo.java
+++ MemberInfo.java
@@ -3,22 +3,15 @@ public class MemberInfo {
     private int graduationYear;
     private boolean goodStanding;
 
-    /**
-     * Constructs a MemberInfo object for the club member with name name,
-     * graduation year gradYear, and standing hasGoodStanding.
-     */
     public MemberInfo(String name, int gradYear, boolean hasGoodStanding) {
         this.name = name;
         this.graduationYear = gradYear;
         this.goodStanding = hasGoodStanding;
     }
 
-    /** Returns the graduation year of the club member. */
     public int getGradYear() {
         return graduationYear;
     }
-
-    /** Returns true if the member is in good standing and false otherwise. */
     public boolean inGoodStanding() {
         return goodStanding;
     }
